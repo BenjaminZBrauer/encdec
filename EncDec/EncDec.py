8e122a6246de51677847cd5139cca6f1d992cc36
IMissGrant <bzbrauer@gmail.com>
2023-07-23 22:13:52 +0200

    major code refactor. TODO randomized ordering + optimize

--- EncDec/EncDec.py
+++ EncDec/EncDec.py
@@ -1,47 +1,65 @@
 """
-############################################################
 Encryption/Decryption Program:
 	-e "YourStringToEncrypt"	  Encrypt a string
 	-d "YourStringToDecrypt"	  Decrypt a string
 	-h or --help			  This text you are reading
-############################################################
+
+The code in encryption/__init__.py is generated on every call to this program.
+- To modify the execution order, edit the "executionOrder" list in this file.
+- To add new functions (and their inversions for decryption), modify "encryption/__init__.py"
+- To expand upon the XMLP functionality, new features can be implemented in "xmlp/__init__.py"
 """
 import sys
-import encryption.encryption as encryptor
+import encryption as encryptor
+from xmlp import xmlPreProcessor as fileParser
+
+executionOrder = [0,1,0,2,5,2,2]
+flag = None
+myParser = fileParser()
+myParser.parseFile("encryption/__init__.py",executionOrder,flag)
 g_Mode = None
 
 def Set_Program_Mode(mode):
 	global g_Mode
-	if (mode[1].lower()=='e'):
-		g_Mode = True
-	elif (mode[1].lower()=='d'):
-		g_Mode = False
-	else:
-		g_Mode = None
-if (len(sys.argv)>1):		
+	g_Mode = mode[1].lower()
+
+if len(sys.argv)==1:
+		print(__doc__)
+		exit()	
+
+if (len(sys.argv)>1):
+	Set_Program_Mode(sys.argv[1])
+
 	if sys.argv[1]=="--help" or sys.argv[1]=="-h":
 		print(__doc__)
-		quit()
+		exit()
+
+	elif g_Mode == 't':
+		testString = "This is a test string :)."
+		print(testString)
+		x = encryptor.functionPipeliner(encryptor.getEncFunctions(),testString)
+		print(x)
+		x = encryptor.functionPipeliner(encryptor.getDecFunctions(),x)
+		print(x)
+		exit()
 
 if (len(sys.argv)==3):
 	if sys.argv[2][0] == '-':
 		sys.argv[1],sys.argv[2] = sys.argv[2], sys.argv[1]
 
-	Set_Program_Mode(sys.argv[1])
-
-	if g_Mode == True:
+	if g_Mode == 'e':
 		x = encryptor.functionPipeliner(encryptor.getEncFunctions(),sys.argv[2])
 		print(x)
-	elif g_Mode == False:
+	elif g_Mode == 'd':
 		x = encryptor.functionPipeliner(encryptor.getDecFunctions(),sys.argv[2])
 		print(x)
 
 	else:
 		print("Unrecognized flag (are you calling \"python EncDec.py [-e or -d] [\"stringToProcess\"]  ?\"")
-		print("quiting...")
-		quit()
+		print("exiting...")
+		exit()
 else:
-	print("EncDec.py accepts exactly 2 arguments.")
-	print("quiting...")
-	quit()
-
+	print("EncDec.py invalid arguments.")
+	print("exiting...")
+	print(__doc__)
+	exit()
---
+++ EncDec/xmlp/__init__.py
@@ -0,0 +1,175 @@
+class xmlPreProcessor:
+	tagsOpen = 0
+	contents = None
+	isReading, isGenerating = False, False
+	currentInstruction = None
+	forward, reverse = [], []
+	flag = None
+	numEncFuncs = 0
+
+	def __removePreviouslyGeneratedCode(self):
+		contents = []
+		skipping = False
+		for i in range(0, len(self.contents)):
+			if not skipping:
+					contents.append(self.contents[i])
+
+			if self.__isInstruction(self.contents[i]):
+				instruction = self.__isInstruction(self.contents[i]).lower()
+				if instruction =='generate':
+					skipping = True
+				elif instruction == '/generate':
+					skipping = False
+					contents.append(self.contents[i])
+				
+		return contents
+
+	def __reverse(self,x):
+		bufferArray = []
+		i = 0
+		while (i<len(x)):
+			bufferArray.insert(0, x[i])
+			i += 1
+		return bufferArray
+
+	def __openTag(self):
+		self.tagsOpen += 1
+
+	def __closeTag(self):
+		self.tagsOpen -= 1
+
+	def __readLine(self,line):
+		if self.__isInstruction(line):
+			self.isReading = False
+		else:
+			line = line.replace(' ', '')
+			line = line.replace('\t', '')
+			line = line.replace('\n', '')
+			if line!='':
+				if line[0:3]=="def":
+					if self.currentInstruction.lower()=="forward":
+						self.forward.append(line[3:line.index('(')])
+						self.numEncFuncs += 1
+					elif self.currentInstruction.lower()=="reverse":
+						self.reverse.append(line[3:line.index('(')])
+
+	def __generateCode(self,file,index,positions):
+		forward, reverse = [], []
+		forward.append("\n# ANY CODE IS THIS BLOCK WILL BE OVERWRITTEN.\n")
+		forward.append("\ndef getEncFunctions():\n\treturn [")
+		reverse.append("\ndef getDecFunctions():\n\treturn [")
+		
+		def __append(array,func,count):
+			array.append("lambda x: "+func+"(x)")
+			array.append(", "+("\n\t\t" if (count+1)%3==0 else ""))
+		
+		if self.flag==None:
+			if positions==None:
+				self.reverse = self.__reverse(self.reverse)
+				for i in range(len(self.forward)):
+					__append(forward, self.forward[i], i)
+
+				for i in range(len(self.reverse)):
+					__append(reverse, self.reverse[i], i)
+			else:
+				reversedPositions = self.__reverse(positions)
+				for i in range(len(positions)):
+					__append(forward, self.forward[positions[i]], i)
+					__append(reverse, self.reverse[reversedPositions[i]], i)
+			#clean up
+			forward.pop()
+			reverse.pop()
+			forward.append("]\n")
+			reverse.append("]\n\n")
+			forward = "".join(forward)
+			reverse = "".join(reverse)
+			generatedCode = "".join([forward,reverse])
+		
+		elif self.flag=='f':
+			if positions==None:
+				self.reverse = self.__reverse(self.reverse)
+				for i in range(len(self.forward)):
+					__append(forward, self.forward[i], i)
+			else:
+				for i in range(len(positions)):
+					__append(forward, self.forward[positions[i]], i)
+			#clean up
+			forward.pop()
+			forward.append("]\n\n")
+			forward = "".join(forward)
+			generatedCode = forward
+		elif self.flag=='r':
+			if positions==None:
+				self.reverse = self.__reverse(self.reverse)
+				for i in range(len(self.reverse)):
+					__append(reverse, self.reverse[i], i)
+			else:
+				reversedPositions = self.__reverse(positions)
+				for i in range(len(positions)):
+					__append(reverse, self.reverse[reversedPositions[i]], i)
+			#clean up
+			reverse.pop()
+			reverse.append("]\n\n")
+			reverse = "".join(reverse)
+			generatedCode = reverse
+		else:
+			raise SyntaxError(" -- Unrecognised flag used (options are 'f' or 'r') -- ")
+
+		
+		#add generated text
+		self.contents.insert(index, generatedCode)
+		f = open(file,"w")
+		f.write("".join(self.contents))
+		f.close()
+
+
+	def __isInstruction(self,line):
+		instruction = []
+		if line[0:2] == "#<":
+			for c in line:
+				if c=='<':
+					self.__openTag();
+				elif  c=='>':
+					self.__closeTag();
+				if self.tagsOpen>0 and (c!='<' and c!='>'):
+					instruction.append(c)
+
+		if self.tagsOpen>0:
+			raise SyntaxError("(an) XML tag(s) is/are open.")
+			quit()
+		return "".join(instruction) if instruction else False
+
+#MAIN
+	def parseFile(self,file,positions = None, flag = None):
+		instructions = []
+		counter = 0
+		self.contents = (open(file, "r")).readlines()
+		self.flag = flag
+
+		for line in self.contents:
+			instruction = []
+			counter += 1
+			if self.isReading:
+				self.__readLine(line)
+			elif self.isGenerating:
+				counter -= 1
+				self.contents = self.__removePreviouslyGeneratedCode()
+				self.__generateCode(file,counter,positions)
+				break
+			else:
+				instruction = self.__isInstruction(line)
+				if instruction:
+					if instruction.lower()=="ignore":
+						continue
+					if instruction[0]!='/':
+						if instruction.lower()!="generate":
+							if (flag=='f' and instruction.lower()=="forward") or (flag=='r' and instruction.lower()=="reverse") or (flag==None):
+								self.isReading = True
+								self.currentInstruction = instruction
+						else:
+							if self.isReading:
+								raise  SyntaxError("generating within a read-block is not allowed")
+							else:
+								self.isGenerating = True
+
+		return len(self.forward)
